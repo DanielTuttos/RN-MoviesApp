662691554f0591ac679a37cb07d45fb3a2c43990
Daniel Romero <tutorialesdanielromero@gmail.com>
2024-03-23 16:03:56 -0500

    :construction: carousel movies nowplaying finished

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,11 +1,20 @@
 import {Text, View} from 'react-native';
-import { useMovies } from '../../hooks/useMovies';
+import {useMovies} from '../../hooks/useMovies';
+import {ScrollView} from 'react-native-gesture-handler';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import { PosterCarousel } from '../../components';
 
 export const HomeScreen = () => {
-  const {} = useMovies();
+  const {top} = useSafeAreaInsets();
+  const {isLoading, nowPlaying} = useMovies();
+  if (isLoading) {
+    return <Text>cargando</Text>;
+  }
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={{marginTop: top + 20, paddingBottom: 30}}>
+        <PosterCarousel movies={nowPlaying} />
+      </View>
+    </ScrollView>
   );
 };
